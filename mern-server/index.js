27d99fbfe6d0c4e8acf89708c62dbbc436515b97
Your <ckiroshan@gmail.com>
2023-11-24 17:47:57 +0530

    Added function to insert book data.

--- mern-server/index.js
+++ mern-server/index.js
@@ -29,12 +29,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Create a collection of documents
+    const bookCollection = client.db("BookInventory").collection("books");
+    
+    // Insert Book to Database
+    app.post("/upload-book", async (req, res) => {
+      const data = req.body;
+      const result = await bookCollection.insertOne(data);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
