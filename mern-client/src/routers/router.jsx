4e1ac01ebbd1337f48f2a468c3b6d596812dfa94
Your <ckiroshan@gmail.com>
2023-11-26 22:28:20 +0530

    Added Shop file with routing.

--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Home from "../home/Home";
+import Shop from "../shop/Shop";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home/>
+      },
+      {
+        path: "/shop",
+        element: <Shop/>
       }
     ]
   },
