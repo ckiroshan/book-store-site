7743364846b372ca26f4a8b6047c0bf94248a85e
Your <ckiroshan@gmail.com>
2023-11-26 22:28:59 +0530

    Added more components with routing.

--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Home from "../home/Home";
 import Shop from "../shop/Shop";
+import Blog from "../components/Blog";
+import About from "../components/About";
+import SingleBook from "../components/SingleBook";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +18,18 @@ const router = createBrowserRouter([
       {
         path: "/shop",
         element: <Shop/>
+      },
+      {
+        path: "/blog",
+        element: <Blog/>
+      },
+      {
+        path: "/about",
+        element: <About/>
+      },
+      {
+        path: "/book/:id",
+        element: <SingleBook/>
       }
     ]
   },
