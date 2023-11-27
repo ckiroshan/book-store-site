ededc4e0f11d62a148a93d2aff4ee89055570f65
Your <ckiroshan@gmail.com>
2023-11-27 19:38:21 +0530

    Specified the HTML and styled the Navbar.

--- mern-client/src/components/Navbar.jsx
+++ mern-client/src/components/Navbar.jsx
@@ -39,6 +39,51 @@ function Navbar() {
     { link: "Blog", path: "/blog" },
   ];
 
+  return (
+    <header className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
+      <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
+        <div className="flex justify-between items-center text-base gap-8">
+          {/* logo */}
+          <Link to="/" className="text-2x1 font-bold text-blue-700 flex items-center gap-2">
+            <FaBlog className="inline-block" />
+            Books
+          </Link>
+
+          {/* Nav-items: Large screen */}
+          <ul className="md:flex space-x-12 hidden">
+            {navItems.map(({ link, path }) => (
+              <Link key={path} to={path} className="block text-base text-black uppercase cursor-pointer hover:text-blue-700">
+                {link}
+              </Link>
+            ))}
+          </ul>
+
+          {/* Button for large devices */}
+          <div className="space-x-12 hidden lg:flex items-center">
+            <button>
+              <FaBarsStaggered className="w-5 hover:text-blue-700" />
+            </button>
+          </div>
+
+          {/* Button for mobile devices */}
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-black focus:outline-none">
+              {isMenuOpen ? <FaXmark className="h-5 w-5 text-black" /> : <FaBarsStaggered className="h-5 w-5 text-black" />}
+            </button>
+          </div>
+
+          {/* Nav-items: Small screen */}
+          <div className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+            {navItems.map(({ link, path }) => (
+              <Link key={path} to={path} className="block text-base text-white uppercase cursor-pointer">
+                {link}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
