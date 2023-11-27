9eac2eaae0d6de773919484d6cf0f732291ed032
Your <ckiroshan@gmail.com>
2023-11-27 19:39:12 +0530

    Added a temporary HTML structure for Home page.

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="">
+      <div className="h-screen">Home</div>
+      <div className="h-screen bg-red-600"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
