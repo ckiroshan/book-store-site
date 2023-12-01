b34edce89153eca6ac9965e63186a3d53b072ef9
Your <ckiroshan@gmail.com>
2023-12-01 22:53:52 +0530

    Imported React icons and formatted Link import.

--- mern-client/src/components/BookCards.jsx
+++ mern-client/src/components/BookCards.jsx
@@ -1,16 +1,15 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Link } from "react-router-dom";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
-// import "./styles.css";
-
 // import required modules
 import { Pagination } from "swiper/modules";
+import { Link } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
 
 function BookCards({ headline, books }) {
   return (
@@ -43,12 +42,21 @@ function BookCards({ headline, books }) {
       >
         {books.map((book) => (
           <SwiperSlide key={book._id}>
-            <Link to="/">
-              <div>
+            <Link to={`/books/${book._id}`}>
+              <div className="relative">
                 <img src={book.imageURL} alt="" />
-              </div>
-              <div>
-                <h3>{book.bookTitle}</h3>
+                <div className="absolute top-3 right-3 bg-blue-600 hover:bg-black p-2 rounded">
+                  <FaCartShopping className="w-4 h-4 text-white" />
+                </div>
+                <div>
+                  <div>
+                    <h3>{book.bookTitle}</h3>
+                    <p>{book.authorName}</p>
+                  </div>
+                  <div>
+                    <p>$10.00</p>
+                  </div>
+                </div>
               </div>
             </Link>
           </SwiperSlide>
