115ee08856d766a9d6c1581ecb74f21f100b97bb
Your <ckiroshan@gmail.com>
2023-12-02 23:12:01 +0530

    Added margins to BookCards component.

--- mern-client/src/components/BookCards.jsx
+++ mern-client/src/components/BookCards.jsx
@@ -17,51 +17,53 @@ function BookCards({ headline, books }) {
       <h2 className="text-5xl text-center font-bold text-black my-5">{headline}</h2>
 
       {/* Cards */}
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper w-full h-full"
-      >
-        {books.map((book) => (
-          <SwiperSlide key={book._id}>
-            <Link to={`/books/${book._id}`}>
-              <div className="relative">
-                <img src={book.imageURL} alt="" />
-                <div className="absolute top-3 right-3 bg-blue-600 hover:bg-black p-2 rounded">
-                  <FaCartShopping className="w-4 h-4 text-white" />
-                </div>
-                <div>
-                  <div>
-                    <h3>{book.bookTitle}</h3>
-                    <p>{book.authorName}</p>
+      <div className="mt-12">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper w-full h-full"
+        >
+          {books.map((book) => (
+            <SwiperSlide key={book._id}>
+              <Link to={`/books/${book._id}`}>
+                <div className="relative">
+                  <img src={book.imageURL} alt="" />
+                  <div className="absolute top-3 right-3 bg-blue-600 hover:bg-black p-2 rounded">
+                    <FaCartShopping className="w-4 h-4 text-white" />
                   </div>
                   <div>
-                    <p>$10.00</p>
+                    <div>
+                      <h3>{book.bookTitle}</h3>
+                      <p>{book.authorName}</p>
+                    </div>
+                    <div className="mb-12">
+                      <p>$10.00</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
