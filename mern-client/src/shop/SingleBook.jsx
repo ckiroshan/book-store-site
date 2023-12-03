a6c48f831bdb135b6d30d1ac76631d270cdb9d0e
Your <ckiroshan@gmail.com>
2023-12-03 18:49:49 +0530

    Customized SingleBook component.

--- mern-client/src/shop/SingleBook.jsx
+++ mern-client/src/shop/SingleBook.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 function SingleBook() {
-  return (
-    <div>SingleBook</div>
-  )
+  const { _id, bookTitle, imageURL } = useLoaderData();
+  return <div className="mt-28 px-4 lg:px-24">
+    <h2>{bookTitle}</h2>
+    <img src={imageURL} className="h-96" alt="" />
+  </div>;
 }
 
-export default SingleBook
+export default SingleBook;
