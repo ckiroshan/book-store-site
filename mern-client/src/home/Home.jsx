586509955b820694df6e7938225da243f2303a85
Your <ckiroshan@gmail.com>
2023-12-03 18:51:13 +0530

    Added static Favorite component to Home Page.

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import BestSellerBooks from "./BestSellerBooks";
+import FavoriteBooks from "./FavoriteBooks";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <BestSellerBooks />
+      <FavoriteBooks />
     </div>
   );
 };
