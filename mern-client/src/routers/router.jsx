55f5b5379b9ffefa6dee8a9171c9ca55bf01d43b
Your <ckiroshan@gmail.com>
2023-12-06 22:12:15 +0530

    Added required routes for admin dashboard.

--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -5,8 +5,14 @@ import Shop from "../shop/Shop";
 import Blog from "../components/Blog";
 import About from "../components/About";
 import SingleBook from "../shop/SingleBook";
+import DashboardLayout from "../dashboards/DashboardLayout";
+import Dashboard from "../dashboards/Dashboard";
+import UploadBook from "../dashboards/UploadBook";
+import ManageBooks from "../dashboards/ManageBooks";
+import EditBooks from "../dashboards/EditBooks";
 
 const router = createBrowserRouter([
+  // User routes
   {
     path: "/",
     element: <App />,
@@ -30,7 +36,32 @@ const router = createBrowserRouter([
       {
         path: "/books/:id",
         element: <SingleBook />,
-        loader:({params}) => fetch(`http://localhost:7000/books/${params.id}`)
+        loader: ({ params }) => fetch(`http://localhost:7000/books/${params.id}`),
+      },
+    ],
+  },
+
+  // Admin routes
+  {
+    path: "/admin/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/admin/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/admin/dashboard/upload",
+        element: <UploadBook />,
+      },
+      {
+        path: "/admin/dashboard/manage",
+        element: <ManageBooks />,
+      },
+      {
+        path: "/admin/dashboard/edit/:id",
+        element: <EditBooks />,
+        loader: ({ params }) => fetch(`http://localhost:7000/books/${params.id}`),
       },
     ],
   },
