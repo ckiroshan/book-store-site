e79f6a372acc0e5bd5d4f693f1defae7c49a907f
Your <ckiroshan@gmail.com>
2023-12-06 22:11:30 +0530

    Created the Dashboard sidebar component.

---
+++ mern-client/src/dashboards/SideBar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Sidebar } from "flowbite-react";
+import { BiBuoy } from "react-icons/bi";
+import { HiChartPie, HiInbox, HiOutlineCloudUpload, HiShoppingBag, HiLogin, HiUser, HiViewBoards, HiLogout } from "react-icons/hi";
+import userImg from "../assets/profile.jpg";
+
+function SideBar() {
+  return (
+    <Sidebar aria-label="Sidebar with content separator example">
+      <Sidebar.Logo href="#" img={userImg} imgAlt="Flowbite logo">
+        Flowbite
+      </Sidebar.Logo>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="/admin/dashboard" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="/admin/dashboard/upload" icon={HiOutlineCloudUpload}>
+            Upload Book
+          </Sidebar.Item>
+          <Sidebar.Item href="/admin/dashboard/manage" icon={HiInbox}>
+            Manage Books
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiLogin}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiLogout}>
+            Log Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Upgrade to Pro
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Documentation
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={BiBuoy}>
+            Help
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
+
+export default SideBar;
